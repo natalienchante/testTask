b8d26a506695fe6a476b865e81763cf81b7ebf8a
Zayankovskaya <n.zayankovskaya@itransition.com>
2017-03-02 17:27:58 +0300

    Add form view and parse template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\ParserForm;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use \KzykHys\CsvParser\CsvParser;
 
 class DefaultController extends Controller
 {
@@ -13,9 +18,19 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $parser = new ParserForm();
+        $form = $this->createFormBuilder($parser)
+            ->add('filename', TextType::class)
+            ->add('file', FileType::class)
+            ->add('add', SubmitType::class)
+            ->getForm();
+
+
+        //$parser = CsvParser::fromFile('./');
+        //$result = $parser->parse();
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/parse.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
